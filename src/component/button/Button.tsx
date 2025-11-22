a2fc4e947469babf04234d0e289acba15ceade66
Febri Wardhana <44961442+Lukuzushiki@users.noreply.github.com>
2025-11-22 12:55:37 +0700

    Slicing Button Component

---
+++ src/component/button/Button.tsx
@@ -0,0 +1,35 @@
+import "../../app/globals.css";
+
+interface ButtonProps {
+  label: string;
+  variant: "primary" | "secondary" | "outline";
+  onClick?: () => void;
+}
+
+const Button = ({ label, variant = "primary", onClick }: ButtonProps) => {
+  const variantStyling = (variant: "primary" | "secondary" | "outline") => {
+    switch (variant) {
+      case "primary":
+        return "bg-primary text-white hover:bg-primary/40";
+      case "secondary":
+        return "bg-white text-black-text hover:bg-grey-text/10";
+      case "outline":
+        return "bg-transparent border border-black-text text-black-text hover:bg-grey-text/10";
+      default:
+        return "rounded-full bg-primary text-white hover:bg-primary/40";
+    }
+  };
+
+  return (
+    <button
+      className={`rounded-full px-[24px] py-[16px] cursor-pointer transform duration-200 ${variantStyling(
+        variant
+      )}`}
+      onClick={onClick}
+    >
+      <p className="text-sm font-medium">{label}</p>
+    </button>
+  );
+};
+
+export default Button;
