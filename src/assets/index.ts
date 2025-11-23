feedd7e72afba8698cf00223dffe3920053eac7f
Febri Wardhana <44961442+Lukuzushiki@users.noreply.github.com>
2025-11-23 12:52:15 +0700

    Slicing Navbar Component

---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+import Logo from "./image/logo.png";
+
+export { Logo };
---
+++ src/component/navbar/Navbar.tsx
@@ -0,0 +1,51 @@
+import "../../app/globals.css";
+import Image from "next/image";
+import Button from "../button/Button";
+import { Logo } from "@/src/assets";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full rounded-full bg-white/10 backdrop-blur-2xl flex justify-between items-center px-[48px] py-[16px]">
+      <div>
+        <Image src={Logo} alt="Logo" />
+      </div>
+      <div className="flex items-center justify-center gap-[32px]">
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm text-white font-medium hover:text-primary hover:font-bold"
+        >
+          Home
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm text-white font-medium hover:text-primary hover:font-bold"
+        >
+          Destination
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm text-white font-medium hover:text-primary hover:font-bold"
+        >
+          Reviews
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm text-white font-medium hover:text-primary hover:font-bold"
+        >
+          FAQ
+        </a>
+      </div>
+      <Button label="Get Started" variant="secondary" />
+    </nav>
+  );
+};
+
+export default Navbar;
